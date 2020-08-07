b83e3371d0b8b26260f6daf9ebc26ed6af510161
sergiofilho <sergiofilho@softbox.com.br>
2020-08-07 00:46:25 -0300

    Configure cron to execute main service in scheduling

--- cron.py
+++ cron.py
@@ -1,18 +1,13 @@
-from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from monitoramento_precos.helpers import get_logger
-
-
-logger = get_logger()
+from app import main
 
 scheduler = BlockingScheduler()
 
 
 @scheduler.scheduled_job('interval', minutes=5)
 def print_data():
-    data = datetime.now()
-    logger.warning(f'JOB CHAMADA {data}')
+    main()
 
 
 scheduler.start()
