18ce4614b244ed8daef1500a335c84cda1210d55
sergiofilho <sergiofilho@softbox.com.br>
2020-08-07 00:21:28 -0300

    Testing new format of the scheduler

---
+++ cron.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from monitoramento_precos.helpers import get_logger
+
+
+logger = get_logger()
+
+scheduler = BlockingScheduler()
+
+
+@scheduler.scheduled_job('interval', minutes='3')
+def print_data():
+    data = datetime.now()
+    logger.warning(f'JOB CHAMADA {data}')
+
+
+scheduler.start()
