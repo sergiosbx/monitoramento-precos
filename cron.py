70b70a3efc833ffc9b91d807e367368e98489417
sergiofilho <sergiofilho@softbox.com.br>
2020-08-08 00:05:04 -0300

    Increase parallel instances

--- cron.py
+++ cron.py
@@ -5,7 +5,7 @@ from app import main
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('interval', minutes=5, max_instances=3)
 def print_data():
     main()
 
